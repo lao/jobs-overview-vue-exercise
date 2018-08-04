2ebc2ff3ba75f740661ad36c1b5e0aeee675fad3
Lucas Oliveira <lucasabreuoliveira@gmail.com>
2018-08-04 15:04:20 +0200

    adding basic app container with vue material

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">My Title</span>
+      </md-app-toolbar>
+
+      <md-app-content>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
+          similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo,
+          neque.</p>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    name: 'Waterfall'
   }
-}
+</script>
+<style lang="scss" scoped>
+  .md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
 </style>
